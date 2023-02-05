c8d5fa8b6c116394a72fb759a358364f6b331fe3
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-02-05 22:42:29 +0500

    "added edit button and contrains checks"

--- assignment-two/src/App.js
+++ assignment-two/src/App.js
@@ -5,11 +5,21 @@ import { useState } from "react";
 function App() {
   const [text, setText] = useState([]);
   const [input, setInput] = useState("");
+  let [edit_val, setEditval] = useState("");
+  const [checkChange, setcheckChange] = useState(true);
+  // let [current_text, setCurrentText] = useState("");
 
   const Add = () => {
-    text.push(input);
-    setText([...text]);
-    setInput("");
+    if (input.length >= 1) {
+      text.push(input);
+      setText([...text]);
+      setInput("");
+      setcheckChange(true);
+    } else {
+      console.log();
+      console.log("input required !", input);
+      alert("input required !");
+    }
   };
   const del = (i) => {
     let removed = text.splice(i, 1);
@@ -17,27 +27,76 @@ function App() {
     setText([...text]);
     setInput("");
   };
+  // let editpnChange = (i) => {
+  //   let current_text = text[i];
+  //   console.log(current_text);
+  //   console.log(edit_val);
+  //   // let bring_change = text.replace(current_text, edit_val);
+  //   // console.log(bring_change);
+  //   // setText([...bring_change]);
+  //   // setEditval("");
+  //   setcheckChange(false);
+  // };
+  let managechange = (i) => {
+    console.log(" At manage change value needs to be edited : ", text[i]);
+
+    setcheckChange(false);
+  };
+
+  let edit = (i) => {
+    let current_text = text[i];
+    console.log("value needs to be edited : ", current_text);
+
+    console.log("value recieved to edit : ", edit_val);
+
+    let bring_change;
+    bring_change = text.map((x) => {return x.replace(current_text, edit_val)});
+
+    console.log(bring_change);
+    setText([...bring_change]);
+    setEditval("");
+    setcheckChange(true);
+  };
+
   return (
     <div className="App">
-      {/* {<button onClick={()=>del(i)}></button>} */}
-      <input
-        placeholder="enter text "
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-        value={input.length === null ? null : input}
-      ></input>
-      <button onClick={Add}>Add Text</button>
+      {checkChange ? (
+        <input
+          placeholder="enter text "
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          value={input}
+        ></input>
+      ) : (
+        <input
+          placeholder="change text "
+          onChange={(e) => {
+            setEditval(e.target.value);
+          }}
+          value={edit_val}
+        ></input>
+      )}
+      {checkChange ? (
+        <button onClick={Add}>Add Text</button>
+      ) : (
+        text.map((x, i) => (
+          <div>
+            <button onClick={() => edit(i)}>Edit</button>
+          </div>
+        ))
+      )}
       <table className="tb">
         <tr>
-          <th>Ids | Todos</th>
+          <th>Ids | Todos | Delete | Edit </th>
         </tr>
         <tr>
           {text.map((x, i) => (
             <div>
               <td>{i} | </td>
               <td>
-                {x} <button onClick={() => del(i)}>Delete</button>
+                {x} | <button onClick={() => del(i)}>Delete</button> |{" "}
+                <button onClick={() => managechange(i)}>Edit</button>
               </td>
             </div>
           ))}
